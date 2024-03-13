4577534bcba0fab69f864c3379697e292da40df1
Richard Hao <richard@feedmob.com>
2024-03-13 15:53:14 +0800

    feat: get ticker name when hovering over a tag

--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -1,1 +1,19 @@
 console.info('contentScript is running')
+
+// Regular expression to match $ followed by uppercase letters and numbers
+const tickerPattern = /\$[A-Z0-9]+\b/;
+
+document.addEventListener('mouseover', (event) => {
+  const target = event.target;
+
+  if (target.tagName === 'A' && tickerPattern.test(target.textContent)) {
+    console.log('mouseover:', target.textContent);
+    const match = target.textContent.match(tickerPattern)[0];
+    console.log('match:', match);
+  }
+});
+
+document.addEventListener('mouseout', (event) => {
+  // const target = event.target;
+  // console.log('mouseout:', target)
+});
