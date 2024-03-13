29b1a1152d74828b9da709f9d1966db71497fb24
Richard Hao <richard@feedmob.com>
2024-03-13 16:21:40 +0800

    feat: use chrome background worker to handler TICKER_INFO event

--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -4,12 +4,13 @@ console.info('contentScript is running')
 const tickerPattern = /\$[A-Z0-9]+\b/;
 
 document.addEventListener('mouseover', (event) => {
-  const target = event.target;
+  const target = event.target
 
   if (target.tagName === 'A' && tickerPattern.test(target.textContent)) {
-    console.log('mouseover:', target.textContent);
-    const match = target.textContent.match(tickerPattern)[0];
-    console.log('match:', match);
+    const ticker = target.textContent.match(tickerPattern)[0]
+    chrome.runtime.sendMessage({ type: 'TICKER_INFO', ticker }, (response) => {
+      console.log('contentScript has received a message from background, and ticker info is ', response?.ticker, response?.price);
+    })
   }
 });
 
