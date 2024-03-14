367c4fc9d96736015439914a0047153be83092d2
Richard Hao <richard@feedmob.com>
2024-03-14 11:21:45 +0800

    chore: only keep background and content_script in manifest.json

--- src/manifest.js
+++ src/manifest.js
@@ -14,12 +14,12 @@ export default defineManifest({
     48: 'img/logo-48.png',
     128: 'img/logo-128.png',
   },
-  action: {
-    default_popup: 'popup.html',
-    default_icon: 'img/logo-48.png',
-  },
-  options_page: 'options.html',
-  devtools_page: 'devtools.html',
+  // action: {
+  //   default_popup: 'popup.html',
+  //   default_icon: 'img/logo-48.png',
+  // },
+  // options_page: 'options.html',
+  // devtools_page: 'devtools.html',
   background: {
     service_worker: 'src/background/index.js',
     type: 'module',
@@ -30,17 +30,17 @@ export default defineManifest({
       js: ['src/contentScript/index.js'],
     },
   ],
-  side_panel: {
-    default_path: 'sidepanel.html',
-  },
+  // side_panel: {
+  //   default_path: 'sidepanel.html',
+  // },
   web_accessible_resources: [
     {
       resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
       matches: [],
     },
   ],
-  permissions: ['sidePanel', 'storage'],
-  chrome_url_overrides: {
-    newtab: 'newtab.html',
-  },
+  // permissions: ['sidePanel', 'storage'],
+  // chrome_url_overrides: {
+  //   newtab: 'newtab.html',
+  // },
 })
