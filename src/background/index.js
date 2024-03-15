ad52afae57dbbcf1f926b2618741fde62544e761
Richard Hao <richard@feedmob.com>
2024-03-15 18:37:59 +0800

    feat: get the symbol quote from the API

--- src/background/index.js
+++ src/background/index.js
@@ -1,5 +1,21 @@
 console.log('background is running')
 
+async function fetchRealtimePrice(ticker) {
+  const symbol = ticker.replace('$', '').toUpperCase()
+  return fetch(`https://sandbox-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${symbol}`, {
+    method: 'GET',
+    headers: {
+      'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+      'Accept': 'application/json',
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('fetchRealtimePrice', data?.data)
+      return data?.data?.[symbol]
+    })
+}
+
 chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
   if (request.type === 'COUNT') {
     console.log('background has received a message from popup, and count is ', request?.count)
@@ -8,8 +24,11 @@ chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
 
   if (request.type === 'TICKER_INFO') {
     console.log('background has received a message from contentScript, and ticker info is ', request?.ticker)
-    sendResponse({ type: 'TICKER_INFO', ticker: request?.ticker, price: 100 })
+    fetchRealtimePrice(request?.ticker).then(data => {
+      console.log('TICKER_INFO', data)
+      sendResponse({ type: 'TICKER_INFO', ticker: request?.ticker, data: data })
+    }).catch(err => console.error(err))
 
-    return
+    return true
   }
 })
--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -8,7 +8,7 @@ console.info('contentScript is running')
 // Regular expression to match $ followed by uppercase letters and numbers
 const tickerPattern = /\$[A-Z0-9]+\b/;
 
-function fetchPriceForTicker(ticker, callback) {
+async function fetchPriceForTicker(ticker, callback) {
   chrome.runtime.sendMessage({ type: 'TICKER_INFO', ticker }, (response) => {
     callback(response)
   })
@@ -34,8 +34,8 @@ document.addEventListener('mouseover', (event) => {
       followCursor: true,
       onShow(instance) {
         fetchPriceForTicker(ticker, (response) => {
-          console.log('contentScript has received a message from background, and ticker info is ', response?.ticker, response?.price);
-          instance.setContent(`Price for ${ticker}: $${response?.price}`);
+          console.log('contentScript has received a message from background, and ticker info is ', response);
+          instance.setContent(`Price for ${ticker}: $${response?.data[0]?.quote?.USD?.price}`);
         });
         tippyInstances.push(instance);
       }
