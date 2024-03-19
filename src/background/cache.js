bb083228a1aef37b2c798f51d5ae361556932dbc
Richard Hao <richard@feedmob.com>
2024-03-19 09:38:00 +0800

    fix: fix the cache issue
    -  and add cache version to the cache key

--- src/background/cache.js
+++ src/background/cache.js
@@ -1,29 +1,37 @@
 const storage = chrome.storage.local
 
+const CACHE_VERSION = 'v1'
+
+function getCacheKey(key) {
+  return `${CACHE_VERSION}_${key}`
+}
+
 export function setCache(key, value, durationInSeconds) {
+  const cacheKey = getCacheKey(key)
   const expiration = new Date()
-  expiration.setSeconds(expiration.getSeconds() + durationInSeconds)
   const cache = {
     data: value,
-    expiration: expiration,
+    expiration: expiration.setSeconds(expiration.getSeconds() + durationInSeconds)
+    ,
   }
-  storage.set({ [key]: cache }, () => {
-    console.log(`Data for ${key} is now cached with expiration.`);
+  storage.set({ [cacheKey]: cache }, () => {
+    console.log(`Data for ${cacheKey} is now cached with expiration.`);
   })
 }
 
 export function getCache(key, callback) {
-  storage.get([key], (data) => {
-    const cache = data[key]
+  const cacheKey = getCacheKey(key)
+  storage.get([cacheKey], (data) => {
+    const cache = data[cacheKey]
     if (!cache) {
       callback(null)
       return
     }
 
     if (new Date(cache.expiration) < new Date()) {
-      console.log(`Cache for ${key} has expired.`);
-      storage.remove([key], () => {
-        console.log(`Cache for ${key} has been removed.`);
+      console.log(`Cache for ${cacheKey} has expired.`);
+      storage.remove([cacheKey], () => {
+        console.log(`Cache for ${cacheKey} has been removed.`);
       })
       callback(null)
       return
