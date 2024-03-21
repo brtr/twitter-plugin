b2a6361218fc5dcfc293dcd7d978131d857f6634
Richard Hao <richard@feedmob.com>
2024-03-21 09:33:11 +0800

    feat: increase cache expiration time to 1 hour

--- src/background/index.js
+++ src/background/index.js
@@ -1,6 +1,8 @@
 import { getCache, setCache } from './cache'
 import { fetchRealtimePrice } from './fetch'
 
+const CACHE_EXPIRATION_IN_SECONDS = 3600 // 1 hour
+
 chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
   if (request.type === 'COUNT') {
     console.log('background has received a message from popup, and count is ', request?.count)
@@ -16,7 +18,7 @@ chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
       } else {
         fetchRealtimePrice(request?.ticker).then(data => {
           console.log('TICKER_INFO', data)
-          setCache(cacheKey, data, 360)
+          setCache(cacheKey, data, CACHE_EXPIRATION_IN_SECONDS)
           sendResponse({ type: 'TICKER_INFO', ticker: request?.ticker, data: data })
         }).catch(err => console.error(err))
       }
