2487661661648acd0cabc6c688170a54ed704598
Richard Hao <richard@feedmob.com>
2024-03-26 17:05:40 +0800

    feat(api): add func to fetchSpotAggregateTransactions

--- src/background/fetch.js
+++ src/background/fetch.js
@@ -3,8 +3,12 @@ async function getApiToken() {
   return apiTokenResult?.CMC_API_TOKEN
 }
 
+function getSymbolFromTicker(ticker) {
+  return ticker.replace('$', '').toUpperCase()
+}
+
 export async function fetchRealtimePrice(ticker) {
-  const symbol = ticker.replace('$', '').toUpperCase()
+  const symbol = getSymbolFromTicker(ticker)
   const apiToken = await getApiToken()
 
   return fetch(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${symbol}`, {
@@ -18,5 +22,21 @@ export async function fetchRealtimePrice(ticker) {
     .then(data => {
       console.log('fetchRealtimePrice', data?.data)
       return data?.data?.[symbol]
+    })
+}
+
+export async function fetchSpotAggregateTransactions(ticker) {
+  const symbol = getSymbolFromTicker(ticker)
+
+  return fetch(`https://spot.techbay.club/aggregate_transactions?search=${symbol}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('fetchSpotTransactions', data)
+      return data?.[0]
     })
 }
